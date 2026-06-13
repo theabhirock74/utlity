2b1c6eb00717b04e00be62441870857a6abe5c12
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2026-06-13 06:35:18 +0000

    add Notes Doctype for common notes storage

---
+++ utlity/utlity/doctype/notes/notes.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, theabhirock74 and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Notes", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ utlity/utlity/doctype/notes/test_notes.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, theabhirock74 and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestNotes(FrappeTestCase):
+	pass
